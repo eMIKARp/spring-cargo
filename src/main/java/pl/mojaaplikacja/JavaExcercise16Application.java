330e5312d73d4f2348a8c333ed33245d6eb09f45
eMIKARp <emil.karpowicz@gmail.com>
2018-11-22 21:23:53 +0100

    Initial commit

---
+++ src/main/java/pl/mojaaplikacja/JavaExcercise16Application.java
@@ -0,0 +1,12 @@
+package pl.mojaaplikacja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExcercise16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExcercise16Application.class, args);
+	}
+}
